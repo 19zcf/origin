222fd66758c6e104670f59baa0390f3502802ca9
郑传坊 <15880087257@163.com>
2025-08-21 09:41:41 +0800

    分模块前的最后一次提交

--- tilias-web-management/src/main/java/com/zcf/TiliasWebManagementApplication.java
+++ tilias-web-management/src/main/java/com/zcf/TiliasWebManagementApplication.java
@@ -2,6 +2,11 @@ package com.zcf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
 
 @SpringBootApplication
 public class TiliasWebManagementApplication {
